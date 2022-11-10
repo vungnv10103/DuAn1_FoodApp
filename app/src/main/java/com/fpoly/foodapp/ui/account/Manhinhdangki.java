b0a30de3ea3b67dec0f89f566a5bed23a16b8513
Admin <cuongtlph27535>
2022-11-10 19:24:40 +0700

    Cuong man hinh dang ki

---
+++ app/src/main/java/com/fpoly/foodapp/ui/account/Manhinhdangki.java
@@ -0,0 +1,16 @@
+package com.fpoly.foodapp.ui.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fpoly.foodapp.R;
+
+public class Manhinhdangki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinhdangki);
+    }
+}
