cd043825c21efcc1e5f8a1a8b0123a414def6b1c
vitttt03 <vietqpq63@gmail.com>
2022-11-30 14:18:11 +0700

    favorite

---
+++ app/src/main/java/com/fpoly/foodapp/adapters/FeaturedVerAdapter.java
@@ -0,0 +1,117 @@
+package com.fpoly.foodapp.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.fpoly.foodapp.R;
+import com.fpoly.foodapp.activities.ShowDetailActivity;
+import com.fpoly.foodapp.modules.FeaturedModule;
+import com.fpoly.foodapp.modules.FeaturedVerModule;
+import com.fpoly.foodapp.modules.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeaturedVerAdapter extends RecyclerView.Adapter<FeaturedVerAdapter.ViewHolder> implements Filterable {
+    private Context context;
+    private List<Food> list;
+    private List<Food> listold;
+
+    public FeaturedVerAdapter(Context context,List<Food> list) {
+        this.context = context;
+        this.list = list;
+        this.listold = list;;
+    }
+
+    @NonNull
+    @Override
+    public FeaturedVerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.featured_ver_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeaturedVerAdapter.ViewHolder holder, int position) {
+        holder.img.setImageResource(list.get(position).getImg());
+        holder.imgAdd.setImageResource(list.get(position).getResource_image());
+        holder.tvTitle.setText(list.get(position).getTitle());
+        holder.tvPrice.setText(String.valueOf(list.get(position).getMoney()));
+        holder.imgAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ShowDetailActivity.class);
+                context.startActivity(intent);
+                Intent intent1 = new Intent(context, ShowDetailActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("image", list.get(position).getImg());
+                bundle.putDouble("price", list.get(position).getMoney());
+                bundle.putString("title", list.get(position).getTitle());
+                intent1.putExtra("data", bundle);
+                context.startActivity(intent1);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String str = constraint.toString();
+                if(str.isEmpty()){
+                    list = listold;
+
+                }
+                else {
+                    List<Food> mlist = new ArrayList<>();
+                    for( Food food :listold){
+                        if(food.getTitle().toLowerCase().contains(str.toLowerCase())){
+                            mlist.add(food);
+                        }
+                    }
+                    list = mlist;
+                }
+
+                FilterResults  filterResults = new FilterResults();
+                filterResults.values = list;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                list = (List<Food>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvPrice;
+        ImageView img, imgAdd;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitleRecommended);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            img = itemView.findViewById(R.id.imgRecommended);
+            imgAdd = itemView.findViewById(R.id.imgAdd);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/fpoly/foodapp/ui/favourite/fragment/FirstFragment.java
@@ -0,0 +1,70 @@
+package com.fpoly.foodapp.ui.favourite.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fpoly.foodapp.R;
+import com.fpoly.foodapp.adapters.FeaturedAdapter;
+import com.fpoly.foodapp.adapters.FeaturedVerAdapter;
+import com.fpoly.foodapp.modules.FeaturedModule;
+import com.fpoly.foodapp.modules.FeaturedVerModule;
+import com.fpoly.foodapp.modules.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FirstFragment extends Fragment {
+///////Hor
+List<FeaturedModule> featuredModules;
+RecyclerView recyclerView;
+FeaturedAdapter featuredAdapter;
+//////Ver
+    List<Food> foods;
+    RecyclerView recyclerView2;
+    FeaturedVerAdapter featuredVerAdapter;
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=  inflater.inflate(R.layout.fragment_first, container, false);
+
+        //////////////hor
+        recyclerView = view.findViewById(R.id.featured_hor_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.HORIZONTAL,false));
+
+        featuredModules = new ArrayList<>();
+        featuredModules.add(new FeaturedModule(R.drawable.fav1,"Featured 1","Description 1"));
+        featuredModules.add(new FeaturedModule(R.drawable.fav2,"Featured 2","Description 2"));
+        featuredModules.add(new FeaturedModule(R.drawable.fav3,"Featured 3","Description 3"));
+
+        featuredAdapter = new FeaturedAdapter(featuredModules);
+        recyclerView.setAdapter(featuredAdapter);
+///////////////ver
+
+        recyclerView2 = view.findViewById(R.id.featured_ver_rec);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false));
+
+        foods = new ArrayList<>();
+        foods.add(new Food(R.drawable.ver1,"Creamy sticky rice",2.5,R.drawable.plus_circle));
+        foods.add(new Food(R.drawable.ver2,"Hamburger",2.8,R.drawable.plus_circle));
+        foods.add(new Food(R.drawable.ver3,"Pasta",2.8,R.drawable.plus_circle));
+
+
+        featuredVerAdapter = new FeaturedVerAdapter(getContext(),foods);
+        recyclerView2.setAdapter(featuredVerAdapter);
+        return view;
+    }
+}
